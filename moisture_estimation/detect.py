ddad81177da692ab92e6c0b82642a3986900e4a2
xiegd <xgd0401@gmail.com>
2024-06-22 10:40:30 +0800

    Modified the prompt output in detect.py

--- moisture_estimation/detect.py
+++ moisture_estimation/detect.py
@@ -22,13 +22,13 @@ while len(line) > 3:
     line = f.readline()
     # print(line)
 f.close()
-print("success")
+print("Sensor Detecting.")
 # record the str enabled,x is 1 or 0
 A_1 = list(0 for i in range(len(A)))
 B_1 = list(0 for i in range(len(B)))
 
 # detected file
-file_path = "/home/pi/Desktop/mercuryapi-1.37.2.24/cs/ThingMagicReader_test.csv"
+file_path = "./ThingMagicReader_test.csv"
 # now line's id
 temp = -1 # it is used to record which serial number was discovered and change subsequent values based on the valuse of that variable
 # record the begin and now frequency . they serve as criteria for terminating records
@@ -60,22 +60,28 @@ while 1:
     if previous_size < current_size :
         # from IF or EOF begin read
         f = open(file_path,'r')
+        # previous_size=previous_size-flag3
         f.seek(previous_size)
         # read 
-        line = f.readline()
+        line = f.readline().replace("\n","")
         # about the data's write
         
         # data store we want
         f2 = open("./moisture_estimation/data/Data" + ".txt",'a') 
-        while len(line) > 3 and len(line.split(',')) == 9:
+        # print(len(line))
+        # break
+        while len(line) > 50 and len(line.split(',')) == 9 and len(line.split(',')[0]) == 4:
+            # print("i am :",line)
+            # print("len:",len(line))
             # becaues the file read may error or split's index isn't 9,we record the important position
-            previous_size = f.tell()
+            previous_size =  f.tell()
+            
             # get the id
             id = line.split(',')[0]
             # begin detect
             if i < MAX:
                 i = i + 1
-                print(i)
+                # print(i)
 
             if id in A :
                 temp = A.index(id)
@@ -90,14 +96,18 @@ while 1:
             if temp != -1 and A_1[temp] and B_1[temp]  and TAG[temp] == -1:
                 # record the now tag begin id
                 TAG[temp] = int(line.split(',')[1])
-                print("detected tag:",temp+1,"its begin is",TAG[temp])
+                print("Detected sensor ID: ", temp + 1)
+                print("----------------")
+                # print("detected tag:",temp+1,"its begin is",TAG[temp])
+              
             # choice
             if flag == 0  and i >= MAX:
-                print("we detected those tags: ",end = " ")
+                print("Stop Detecting.")
+                print("Detected those tags: ",end = " ")
                 for k in range(0,len(A)):
                     if(TAG[k] != -1):
                         print(k+1,end="  ")
-                print("\nplease input the tag that you want:",end = " ")
+                print("\nPlease input the sensor ID that you want to save its data:",end = " ")
                 # input the tag you want
                 tag = int(input())
                 flag = 1
@@ -116,16 +126,19 @@ while 1:
                     print("data source:",line.replace("\n","").split(",")[0])
                     print("---writting:",tag,"---")
                     loop = 1
+                    print("loop:",loop,"loop2:",loop2)
                     f2.write(line+"\n")
                 elif loop == 1 and now == begin :
                     print("data source:",line.replace("\n","").split(",")[0])
                     print("---writting:",tag,"---")
                     loop2 = 1
+                    print("loop:",loop,"loop2:",loop2)
                     f2.write(line+"\n")
-                elif  loop2 == 1  and (abs(now-begin) == 250 or begin == 924375) and temp == tag - 1:
+                elif  loop2 == 1 and (now != begin) and temp == tag - 1: #and (abs(now-begin) == 250 or begin == 924375) and temp == tag - 1:
                     # stop write and exit the process and close the file 
-                    print("Frequency:",now)
+                    # print("Frequency:",now)
                     print("---ending:",tag,"---")
+                    print("loop:",loop,"loop2:",loop2)
                     f.close()
                     f2.close()
                     # use the tag as file name
@@ -133,13 +146,17 @@ while 1:
                     end_time = time.time()
                     print("total time: ", end_time - start_time)
                     exit()
-
-            line = f.readline()
+# python ./moisture_estimation/predict.py 19
+            line = f.readline().replace("\n","")
             temp = -1
         
         
         f.close()
         f2.close()
-        print("----------------")
-            
+        
+        # print("----------------")
+        # print(line)
+        previous_size=previous_size+len(line)+1
+        # print("指针后移，长度:",len(line))
+#f3.close()        
  
